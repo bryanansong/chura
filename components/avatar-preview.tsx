d6efe5fea63085d38eff0cbec47c3f14cd4d2d03
Bryan <bryanansong2003@gmail.com>
2025-02-11 03:49:12 +0100

    Added the images of face components

--- components/avatar-preview.tsx
+++ components/avatar-preview.tsx
@@ -27,13 +27,50 @@ export function AvatarPreview({ selectedOptions }: AvatarPreviewProps) {
             className="h-full w-full object-contain"
           />
         )}
-        {selectedOptions.eyes && (
-          <svg
-            viewBox="0 0 100 100"
-            style={{ position: "absolute", top: 0, left: 0 }}
-          >
-            {/* Eyes SVG content */}
-          </svg>
+        {selectedOptions["facial-hair"] && (
+          <img
+            src={
+              featureOptions["facial-hair"].filter(
+                (val) => val.id === selectedOptions["facial-hair"]
+              )[0].imageUrl
+            }
+            alt={
+              featureOptions["facial-hair"].filter(
+                (val) => val.id === selectedOptions["facial-hair"]
+              )[0].label || "Option image"
+            }
+            className="absolute ml-3 h-2/5 w-1/2 mt-52 object-contain"
+          />
+        )}
+        {selectedOptions["facial-expression"] && (
+          <img
+            src={
+              featureOptions["facial-expression"].filter(
+                (val) => val.id === selectedOptions["facial-expression"]
+              )[0].imageUrl
+            }
+            alt={
+              featureOptions["facial-expression"].filter(
+                (val) => val.id === selectedOptions["facial-expression"]
+              )[0].label || "Option image"
+            }
+            className="absolute ml-14 mt-14 w-3/5 h-1/2 object-contain"
+          />
+        )}
+        {selectedOptions.accessories && (
+          <img
+            src={
+              featureOptions["accessories"].filter(
+                (val) => val.id === selectedOptions["accessories"]
+              )[0].imageUrl
+            }
+            alt={
+              featureOptions["accessories"].filter(
+                (val) => val.id === selectedOptions["accessories"]
+              )[0].label || "Option image"
+            }
+            className="absolute inset-0 h-full w-full object-contain"
+          />
         )}
       </div>
       <div className="flex flex-col gap-10 min-w-60">
